24b0acd1181dc36362482bee877df221c09ab1c9
Frank Ibem <soulsolja@yahoo.com>
2016-10-31 12:57:15 -0500

    Add project files.

---
+++ UniWatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UniWatch.Startup))]
+namespace UniWatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
