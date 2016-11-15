9ccbd7bd2334094372e2728b797800ad29c8714f
Patrick Tone <tone@thepat.xyz>
2016-11-15 07:13:34 -0600

    Added more classes to the database seed.

--- UniWatch/Models/AppDbInitializer.cs
+++ UniWatch/Models/AppDbInitializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using RestSharp.Extensions;
@@ -11,13 +12,29 @@ namespace UniWatch.Models
 {
     public class AppDbInitializer : DropCreateDatabaseIfModelChanges<AppDbContext>
     {
-        // TODO: Add the managers here
+        private AppDbContext _context;
         private ApplicationUserManager _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private string _pwd;
 
         protected override void Seed(AppDbContext context)
         {
-            // TODO: Initialize the managers here
-            _userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            _context = context;
+            _userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(_context));
+            _roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_context));
+            _pwd = WebConfigurationManager.AppSettings["TestPassword"];
+
+            // Create user roles if they do not exist
+            const string teacherRole = "Teacher";
+            const string studentRole = "Student";
+            var roles = new List<string> {"Admin", teacherRole, studentRole };
+            foreach (var role in roles)
+            {
+                if (!_roleManager.RoleExists(role))
+                {
+                    _roleManager.Create(new IdentityRole(role));
+                }
+            }
 
             // Create some teachers
             var teacherList = new List<ApplicationUser>
@@ -33,6 +50,8 @@ namespace UniWatch.Models
             //    new Teacher() { FirstName = "No", LastName = "Classes", Identity = teacherList[1] },
             //    new Teacher() { FirstName = "Some", LastName = "Classes", Identity = teacherList[2] },
             //};
+
+            //CreateUsers<Teacher>(teachers, teacherRole);
 
             // Create some students
             var studentList = new List<ApplicationUser>
@@ -52,11 +71,47 @@ namespace UniWatch.Models
 
             };
 
-            // Save the accounts in the database
-            //CreateUsers<Teacher>(teachers);
-            CreateUsers<Student>(students);
+            CreateUsersAndAddToRole<Student>(students, studentRole);
 
-            // TODO: Initiailze database here
+            // Create some classes
+            var classes = new List<Class>
+            {
+                new Class() { Name = "Class 0", Number = 0, Section = "All", /*Teacher = teachers[0]*/},
+                new Class() { Name = "Class 1", Number = 1, Section = "Some", /*Teacher = teachers[3]*/},
+                new Class() { Name = "Class 2", Number = 2, Section = "Another Sect", /*Teacher = teachers[0]*/}
+            };
+
+            _context.Classes.AddRange(classes);
+            _context.SaveChanges();
+
+            // Enroll some students in some classes
+            var yesterday = DateTime.Today.AddDays(-1);
+            var lastWeek = DateTime.Today.AddDays(-7);
+
+            var enrollment = new List<Enrollment>
+            {
+                new Enrollment() { Student = students[0], Class = classes[0], EnrollDate = lastWeek },
+                new Enrollment() { Student = students[0], Class = classes[1], EnrollDate = lastWeek },
+                new Enrollment() { Student = students[1], Class = classes[0], EnrollDate = yesterday },
+                new Enrollment() { Student = students[2], Class = classes[0], EnrollDate = yesterday }
+            };
+
+            _context.Enrollments.AddRange(enrollment);
+            _context.SaveChanges();
+
+            // Create some facial profiles for the students
+            var facialProfiles = new List<FacialProfile>
+            {
+                new FacialProfile() { Student = students[0], RecognizerTrained = false },
+                new FacialProfile() { Student = students[1], RecognizerTrained = false },
+                new FacialProfile() { Student = students[2], RecognizerTrained = false },
+                new FacialProfile() { Student = students[3], RecognizerTrained = false }
+            };
+
+            _context.FacialProfiles.AddRange(facialProfiles);
+            _context.SaveChanges();
+
+            // Initiailze database here
             base.Seed(context);
         }
 
@@ -69,18 +124,19 @@ namespace UniWatch.Models
         /// <param name="users">
         /// The users to add to the database
         /// </param>
-        private void CreateUsers<T>(IEnumerable<User> users) where T : class
+        /// <param name="role">
+        /// The user's role
+        /// </param>
+        private void CreateUsersAndAddToRole<T>(IEnumerable<User> users, string role) where T : class
         {
-            using (var db = new AppDbContext())
+            foreach (var user in users)
             {
-                foreach (var user in users)
-                {
-                    _userManager.Create(user.Identity);
-                    db.Set<T>().Add(user as T);
-                }
-
-                db.SaveChanges();
+                _userManager.Create(user.Identity, _pwd);
+                _userManager.AddToRole(user.Identity.Id, role);
+                _context.Set<T>().Add(user as T);
             }
+
+            _context.SaveChanges();
         }
     }
 }
