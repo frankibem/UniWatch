29011ed77d0acb65512b5a3fbaf054a1aa1cbe48
jhernandez8 <joshua.hernandez@ttu.edu>
2016-11-18 13:01:04 -0600

    Implemented the Create View and Class controller for Teachers

---
+++ UniWatch/Controllers/ClassController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniWatch.DataAccess;
+using UniWatch.Models;
+
+namespace UniWatch.Controllers
+{
+    public class ClassController : Controller
+    {
+        private IClassManager _classManager;
+
+        public ClassController()
+        {
+            _classManager = new ClassManager();
+        }
+
+        //GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Number,Section,Semester,Year,Teacher")] Class @class)
+        {
+            _classManager.CreateClass(@class.Name, @class.Number, @class.Section, @class.Semester, @class.Year,@class.Teacher.Id);
+            return View(@class);
+        }
+
+        //GET: Delete
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        //POST: Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int classId)
+        {
+            _classManager.DeleteClass(classId);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
